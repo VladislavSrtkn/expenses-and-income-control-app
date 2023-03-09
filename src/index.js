29173c13db71e13074680e331d34947e14c93e79
VladislavSrtkn <vladislavsrtkn@gmail.com>
2023-03-09 16:30:53 +0100

    added hash-router for correct routing on GHpages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -20,7 +20,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<App />}>
             <Route index element={<MainPage />} />
@@ -34,7 +34,7 @@ root.render(
             </Route>
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
